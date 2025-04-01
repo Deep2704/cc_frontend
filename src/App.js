2998a0b5fd880104c9fc623dd04c0e9956b8091b
deepprabhu <deepprabhu1995@gmail.com>
2025-04-01 22:58:19 +1100

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Welcome from "./components/Welcome/Welcome";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [userState, setUserState] = useState(null);
+
+  useEffect(() => {
+    console.log("Current user state in App:", userState);
+  }, [userState]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              userState ? (
+                <Welcome user={userState} setUserState={setUserState} />
+              ) : (
+                <Login setUserState={setUserState} />
+              )
+            }
+          />
+          <Route path="/login" element={<Login setUserState={setUserState} />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/welcome" element={<Welcome user={userState} setUserState={setUserState} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
